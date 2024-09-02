0fd0d3a452f2f34c311c49d9af79cae423154c51
Gustavo Xudré <gxudre@gmail.com>
2024-09-02 20:00:25 -0300

    Adicionando pratica04

---
+++ praticas/pratica04/src/App.jsx
@@ -0,0 +1,13 @@
+import "./app.css";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div className="card">
+      <Header/>
+      <p>Este é um parágrafo em JSX!</p>
+    </div>
+  );
+};
+
+export default App;
