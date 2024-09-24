6b26299bb1cda71e766dc16569f25061a508ef4d
Gustavo Xudré <gxudre@gmail.com>
2024-09-23 21:53:48 -0300

    Adicionando aula06 react-router-dom

---
+++ aulas/aula06/src/pages/Erro404.jsx
@@ -0,0 +1,7 @@
+const Erro404 = () => {
+    return(
+        <h1>Erro 404. Página não encontrada!</h1>
+    )
+}
+
+export default Erro404;
---
+++ aulas/aula06/src/pages/Perfil.jsx
@@ -0,0 +1,9 @@
+const Perfil = () => {
+  return (
+    <>
+      <h2>Perfil do usuário</h2>
+    </>
+  );
+};
+
+export default Perfil;
